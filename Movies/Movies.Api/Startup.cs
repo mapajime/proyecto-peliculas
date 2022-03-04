ba91ba5e3c042410d9b7aaefd5734632fbb148fc
jimena <mapagabi@hotmail.com>
2022-03-03 21:11:40 -0500

    Se adiciono el Proyecto de Movies.Api

---
+++ Movies/Movies.Api/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Movies.Business.Implementations;
+using Movies.Business.Interfaces;
+using Movies.DataAccess.Context;
+using Movies.DataAccess.Repositories.Implementation;
+using Movies.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddDbContext<MovieContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("movies")));
+            services.AddScoped<IActorRepository, ActorRepository>();
+            services.AddScoped<ICountryRepository, CountryRepository>();
+            services.AddScoped<ILanguageRepository, LanguageRepository>();
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<IGenderRepository, GenderRepository>();
+            services.AddScoped<IMovieGenderRepository, MovieGenderRepository>();
+            services.AddScoped<IActorBusiness, ActorBusiness>();
+            services.AddScoped<ICountryBusiness, CountryBusiness>();
+            services.AddScoped<ILanguageBusinesscs, LanguageBusiness>();
+            services.AddScoped<IMovieGenderBusiness, MovieGenderBusiness>();
+            services.AddScoped<IMovieBusiness, MovieBusiness>();
+            services.AddScoped<IGenderBusiness, GenderBusiness>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
