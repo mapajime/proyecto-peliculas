c2e175605ee9213aca63b004a67b66c01dd898d0
jimena <mapagabi@hotmail.com>
2022-03-16 17:20:53 -0500

    Se adiciona el proyecto web

---
+++ Movies/Movies.Web/Controllers/MovieGenderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Web.Controllers
+{
+    public class MovieGenderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
